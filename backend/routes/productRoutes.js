6b7d56480c5fbb59902c794f6d03a29932b6dc6a
johnryo <jpogge55@gmail.com>
2023-05-05 13:10:15 -0700

    Cut product controller logic from routes into separate controller file; set up redux store and root API slice

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,27 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/Product.js';
 
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  })
-);
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
 
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      return res.status(200).json(product);
-    } else {
-      res.status(404);
-      throw new Error('Resource not found');
-    }
-  })
-);
+router.route('/').get(getProducts);
+
+router.route('/:id').get(getProductById);
 
 export default router;
