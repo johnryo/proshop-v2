6d4ffd9c4ed0ce5c4e3ccf4dc3e748b52799e2f8
johnryo <jpogge55@gmail.com>
2023-05-07 19:05:47 -0700

    Edit error output text

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -17,11 +17,11 @@ const protect = asyncHandler(async (req, res, next) => {
     } catch (error) {
       console.log(error);
       res.status(401);
-      throw new Error('Not authorized - token failed');
+      throw new Error('Not authorized: token failed');
     }
   } else {
     res.status(401);
-    throw new Error('Not authorized - no token');
+    throw new Error('Not authorized: no token');
   }
 });
 
