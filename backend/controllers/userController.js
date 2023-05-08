9c2001413a6cc19147e5809a332fb5d39b78237c
johnryo <jpogge55@gmail.com>
2023-05-07 22:49:29 -0700

    Complete register endpoint and encryption; complete get user profile and update user profile functions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
-import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 import asyncHandler from '../middleware/asyncHandler.js';
+import generateToken from '../utils/generateToken.js';
 
 // POST /api/users/auth - authenticate & get token
 const authUser = asyncHandler(async (req, res) => {
@@ -8,19 +8,9 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
-    });
+    generateToken(res, user._id);
 
-    // Set JWT as HTTP-only cookie
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
-    });
-
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -34,7 +24,28 @@ const authUser = asyncHandler(async (req, res) => {
 
 // POST /api/users - register user
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(201).send('register user');
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({ name, email, password });
+  if (user) {
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 // POST /api/users/logout - logout user & clear cookie
@@ -48,12 +59,45 @@ const logoutUser = asyncHandler(async (req, res) => {
 
 // GET /api/users/profile - get user profile
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).send('get user profile');
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('User not found');
+  }
 });
 
 // PUT /api/users/profile - update user profile
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).send('update user profile');
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('User not found');
+  }
 });
 
 // GET /api/users - get all users - ADMIN
